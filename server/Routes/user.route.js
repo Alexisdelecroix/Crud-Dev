d178228d7992380f601247a3b77ef1d1e3c6cb0a
Delecroix <d-alexis@hotmail.fr>
2023-07-25 10:35:12 +0200

    Mise en place du controller update et delete

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -4,8 +4,11 @@ const userController = require('../controllers/user.controller');
 // register a new user
 router.post('/register', userController.createUser);
 
-// // update
-// router.put('/:id', userController.getAllUsers)
+// update
+router.put('/:id', userController.updateUser)
+
+// delete
+router.delete('/:id', userController.deleteUser)
 
 
 module.exports = router;
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -30,9 +30,109 @@ const createUser = (req, res) => {
     })
 }
 
+const updateUser = (req, res) => {
+    // Extraction de l'ID de l'utilisateur à partir des paramètres de la route
+    const userId = req.params.id;
 
+   // Extraction des données du corps de la requête (nom, prénom, adresse, etc...)
+    const { nom, prenom, adresse, ville, code_postal, telephone, email } = req.body;
+
+    // Vérifier si l'ID de l'utilisateur est présent dans les paramètres de la route
+    if (!userId) {
+        return res.status(400).json({
+            error: 'ID de l\'utilisateur manquant dans les paramètres de la route',
+        });
+    }
+
+    // Vérifier si au moins une des propriétés à mettre à jour est fournie dans le corps de la requête
+    if (!nom && !prenom && !adresse && !ville && !code_postal && !telephone && !email) {
+        return res.status(400).json({
+            error: 'Au moins une propriété doit être fournie pour mettre à jour l\'utilisateur',
+        });
+    }
+
+    // Construction de la requête SQL pour mettre à jour l'utilisateur
+    let query = 'UPDATE `users` SET';
+
+    // Tableau pour stocker les valeurs à mettre à jour
+    const valuesToUpdate = [];
+
+    // Vérifier chaque propriété et ajouter à la requête SQL si elle est fournie
+    if (nom) {
+        query += ' `nom` = ?,';
+        valuesToUpdate.push(nom);
+    }
+    if (prenom) {
+        query += ' `prenom` = ?,';
+        valuesToUpdate.push(prenom);
+    }
+    if (adresse) {
+        query += ' `adresse` = ?,';
+        valuesToUpdate.push(adresse);
+    }
+    if (ville) {
+        query += ' `ville` = ?,';
+        valuesToUpdate.push(ville);
+    }
+    if (code_postal) {
+        query += ' `code_postal` = ?,';
+        valuesToUpdate.push(code_postal);
+    }
+    if (telephone) {
+        query += ' `telephone` = ?,';
+        valuesToUpdate.push(telephone);
+    }
+    if (email) {
+        query += ' `email` = ?,';
+        valuesToUpdate.push(email);
+    }
+
+    // Supprimer la virgule finale de la requête SQL
+    query = query.slice(0, -1);
+
+    // Ajouter la condition pour filtrer par l'ID de l'utilisateur
+    query += ' WHERE `id` = ?';
+
+    // Ajouter l'ID de l'utilisateur aux valeurs à mettre à jour
+    valuesToUpdate.push(userId);
+
+    // Exécuter la requête SQL pour mettre à jour l'utilisateur
+    conn.query(query, valuesToUpdate, (err) => {
+        if (err) {
+            console.error('Erreur lors de la mise à jour de l\'utilisateur');
+            res.status(500).json({ error: 'Erreur lors de la mise à jour de l\'utilisateur' });
+        } else {
+            res.status(200).json({ message: 'Utilisateur mis à jour' });
+        }
+    });
+};
+
+
+const deleteUser = (req, res) => {
+  // Extraction de l'ID de l'utilisateur à partir des paramètres de la route
+  const userId = req.params.id;
+
+   // Vérifier si l'ID de l'utilisateur est présent dans les paramètres de la route
+   if (!userId) {
+       return res.status(400).json({
+           error: 'ID de l\'utilisateur manquant dans les paramètres de la route',
+       });
+   }
+   // Construction de la requête SQL pour supprimer l'utilisateur
+   let query = `DELETE FROM users WHERE users.id = ${userId}`;
+
+   // Exécuter la requête SQL pour mettre à jour l'utilisateur
+   conn.query(query, (err) => {
+       if (err) {
+           console.error('Erreur lors de la suppression de l\'utilisateur');
+           res.status(500).json({ error: 'Erreur lors de la suppression de l\'utilisateur' });
+       } else {
+           res.status(200).json({ message: 'Utilisateur supprimer' });
+       }
+   });
+};
 
 
 module.exports = { 
-    createUser
+    createUser, updateUser, deleteUser
 }
